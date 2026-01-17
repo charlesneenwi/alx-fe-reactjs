b6ac0aad5aa14c2322bb593c4a46abb55efbcc9d
charles neenwi <charlesneenwi@gmail.com>
2026-01-17 22:55:58 +0100

    style components in react app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ border: '1px solid gray'}}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+    )
+}
+
+export default MainContent

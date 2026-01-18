6db3e0c49f50d508e71efbe7183e46ce93b76ba7
charles neenwi <charlesneenwi@gmail.com>
2026-01-18 23:47:31 +0100

    Build a Simple Company Website with React

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: "10px", backgroundColor: "#eee", textAlign: "center" }}>
+      &copy; {new Date().getFullYear()} My Company. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;

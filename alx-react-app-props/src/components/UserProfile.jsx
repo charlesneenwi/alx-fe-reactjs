384bced5d17298bd52ff491f7936fd18329bbd19
charles neenwi <charlesneenwi@gmail.com>
2026-01-18 23:16:23 +0100

    Refactor UserProfile to consume UserContext with useContext

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,15 @@
-const UserProfile = (props) => {
-   return (
-     <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-       <h2 style={{ color: 'blue' }}>{props.name}</h2>
-       <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span> </p>
-       <p>Bio: {props.bio}</p>
-     </div>
-   );
-};
- 
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
 export default UserProfile;

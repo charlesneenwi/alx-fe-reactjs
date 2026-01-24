58190299bd8355b2feae01d735e99b21ba1e7764
charles neenwi <charlesneenwi@gmail.com>
2026-01-24 21:32:50 +0100

    feat: add recipe store using Zustand

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import RecipeList from "./components/RecipeList"
+import AddRecipeForm from "./components/AddRecipeForm"
+
+function App() {
+  
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+      
+    </>
+  )
+}
+
+export default App

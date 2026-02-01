168e576a16901e5e9c5a8b4253e2c0c667569096
charles neenwi <charlesneenwi@gmail.com>
2026-02-01 02:31:56 +0100

    chore: prepare for production deployment

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Search />
 
         <Routes>
-          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/" element={<h1></h1>} />
           <Route path="/about" element={<h1>About Page</h1>} />
         </Routes>
       </main>
